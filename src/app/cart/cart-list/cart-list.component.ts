fde2c0a29bc8d30e43256763ab5b49b3146124ba
Mykhailo Saveliev <Mykhailo_Saveliev@epam.com>
2017-04-21 11:35:06 +0300

    removed specs

--- src/app/cart/cart-list/cart-list.component.ts
+++ src/app/cart/cart-list/cart-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { BasketItem } from '../../shared/basket-item';
-import { BasketService } from '../../shared/basket.service';
+import { BasketItem } from 'app/shared/basket-item';
+import { BasketService } from 'app/shared/basket.service';
 
 @Component({
   selector: 'app-cart-list',
