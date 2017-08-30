e48dbd9d7a430047d8a6a439a7e014cc65a07fd6
Mykhailo Saveliev <Mykhailo_Saveliev@epam.com>
2017-08-30 12:28:24 +0300

    updated index to avoid circular references

--- src/common/services/do-action-strategies/index.ts
+++ src/common/services/do-action-strategies/index.ts
@@ -1,6 +1,1 @@
 export * from './do-action-factory';
-export * from './relogin-strategy';
-export * from './login-strategy';
-export * from './logout-strategy';
-export * from './undefined-strategy';
-export * from './unsupported-strategy';
